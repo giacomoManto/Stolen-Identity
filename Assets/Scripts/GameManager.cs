ea7a22cbcf978a87e6753ee61840b3d75e84663f
Stephen Williams <stephen.williamsb@gmail.com>
2025-02-01 18:41:56 -0500

    Forgot that game manager was your thing, sorry. worked on it a little bit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,16 +10,33 @@ public class GameManager : MonoBehaviour
     TMP_InputField playerInput;
 
     [SerializeField]
+    TMP_Text roomDescription;
+
+    [SerializeField]
     RoomBehavior currentPlayerRoom;
 
     [SerializeField]
-    List<RoomBehavior> allRooms = new List<RoomBehavior>();
+    IDCard currentPlayerID = IDCard.Patient;
+   
+
+    [SerializeField]
+    Dictionary<string, RoomBehavior> allRooms = new Dictionary<string, RoomBehavior>();
     void Start()
     {
-        
+        //Grab all rooms
+        IEnumerable<RoomBehavior> rooms = FindObjectsOfType<MonoBehaviour>().OfType<RoomBehavior>();
+        List<RoomBehavior>temp = new List<RoomBehavior>(rooms);
+        foreach (RoomBehavior room in temp)
+        {
+            allRooms[room.name] = room; 
+        }
+        //Set current player room to starting room, this should be changed on load
+        currentPlayerRoom = allRooms["Starting Room"];
+        //Set room description
+        roomDescription.text = currentPlayerRoom.GetRoomDescription(currentPlayerID.Name);
     }
 
-    private void readPlayerInput()
+    public void readPlayerInput()
     {
         print(playerInput.text);
     }
