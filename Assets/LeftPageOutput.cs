63ec0838eb81c29883e389d0c80de64442e5f445
Stephen Williams <stephen.williamsb@gmail.com>
2025-03-10 00:24:17 -0400

    started work on art portion

--- Assets/LeftPageOutput.cs
+++ Assets/LeftPageOutput.cs
@@ -1,12 +1,22 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class LeftPageOutput : MonoBehaviour
 {
+    [SerializeField]
+    [Tooltip("Room Names that corrispond to the room sprites of the same index.")]
+    private List<string> correspondingRoomNames = new List<string>();
+    [SerializeField]
+    private List<Sprite> allRoomSprites = new List<Sprite>(1);
+    
 
     private static LeftPageOutput instance;
 
     private TMP_Text viewableText;
+    private UnityEngine.UIElements.Image roomSketch;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
@@ -19,6 +29,7 @@ public class LeftPageOutput : MonoBehaviour
             Destroy(gameObject);
         }
         viewableText = GetComponentInChildren<TMP_Text>();
+        roomSketch = GetComponentInChildren<UnityEngine.UIElements.Image>();
     }
     public static LeftPageOutput Instance()
     {
@@ -28,5 +39,14 @@ public class LeftPageOutput : MonoBehaviour
     public void updatePage(string roomName)
     {
         viewableText.text = roomName;
+        //if (allRoomSketches.ContainsKey(roomName))
+        //{
+        //    roomSketch.sprite = allRoomSketches[roomName];
+        //}
+        //else
+        //{
+        //    roomSketch.sprite = allRoomSketches["default"];
+        //}
+        
     }
 }
